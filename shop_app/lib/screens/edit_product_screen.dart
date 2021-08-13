113f38d3adecb381a9e74aab24e3d65681726a6a
vtrfaas <vitor.f.arantes@gmail.com>
2021-08-13 13:32:41 -0300

    Inserimos um regex para validar as urls do campo da imagem, usamos uma global key e associamos com o Form, dessa forma fizemos a validação e o salvamento dos campos usando um objeto Produto de referência que foi atualizado com a instância de novos objetos Produtos e colocamos a propriedade onSaved nos TextFormFieldspara que o conteudo desses campos sejam capturados com a execução dessa método com a ajuda da globalkey

--- shop_app/lib/screens/edit_product_screen.dart
+++ shop_app/lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../providers/product.dart';
+
 class EditProductScreen extends StatefulWidget {
   const EditProductScreen({Key? key}) : super(key: key);
   static const routeName = '/edit-products';
@@ -16,6 +18,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlController = TextEditingController();
 
   final _imageUrlFocusNode = FocusNode();
+
+  //key para o Form
+  final _form = GlobalKey<FormState>();
+
+  //para salvar o conteudo do Form, precisamos de um objeto Product
+  var _editedProduct = Product(
+    id: '',
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+  );
 
   @override
   void initState() {
@@ -36,10 +50,45 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _imageUpdateUrl() {
-    //se a imagem perdeu o foco
-    if (!_imageUrlFocusNode.hasFocus) {
-      setState(() {});
+    //perceba o r antes das quotes, representa um regular expression pattern
+    var urlPattern =
+        r"(https?|ftp)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
+    //uso da classe regex
+    var result = new RegExp(urlPattern, caseSensitive: false)
+        .firstMatch(_imageUrlController.text);
+    if (result == null) {
+      print('URL inválida');
+      return;
     }
+    setState(() {});
+    //se a imagem perdeu o foco
+    // if (!_imageUrlFocusNode.hasFocus) {
+    //   if ((!_imageUrlController.text.startsWith('http') ||
+    //           !_imageUrlController.text.startsWith('https')) ||
+    //       (!_imageUrlController.text.endsWith('.jpg') &&
+    //           !_imageUrlController.text.endsWith('.png') &&
+    //           !_imageUrlController.text.endsWith('jpeg'))) {
+    //     return;
+    //   }
+  }
+
+  //definimos esse método para capturar as informações do form
+
+  //para capturar as informações do form, vamos usar uma globalkey
+  //e atrela-la ao form.
+  void _saveForm() {
+    //executa o validator de todos os campos do form
+    var isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    //o método save é fornecido pelo state object da widget
+    //ele irá executar um método em cada textformfield
+    _form.currentState!.save();
+    print('${_editedProduct.title}');
+    print('${_editedProduct.description}');
+    print('${_editedProduct.price}');
+    print('${_editedProduct.imageUrl}');
   }
 
   @override
@@ -47,10 +96,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            onPressed: _saveForm,
+            icon: Icon(Icons.save),
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: [
               //não precisa definir um controller, pois este text
@@ -64,6 +120,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   //aponta para o próximo focus, quando o softkeyboard for pressionado
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Digite um título';
+                  }
+                  return null;
+                },
+                //atualizamos o produto, com um novo produto e o valor do campo
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: value as String,
+                    description: _editedProduct.description,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Price'),
@@ -75,6 +147,27 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: _editedProduct.title,
+                    description: _editedProduct.description,
+                    price: double.parse(value!),
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Insira um número';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Digite um número válido';
+                  }
+                  if (double.parse(value) <= 0) {
+                    return 'Digite um valor maior que zero.';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
@@ -84,6 +177,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 //aqui não teremos a troca de foco, pois não sabemos quando o usuário
                 //irá trocar de campo
                 focusNode: _descriptionFocusNode,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Insira uma descrição';
+                  }
+                  if (value.length < 10) {
+                    return 'Insira uma descrição com mais de 10 caracteres';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: _editedProduct.title,
+                    description: value as String,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -114,6 +225,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       onEditingComplete: () {
                         setState(() {});
                       },
+
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                          id: '',
+                          title: _editedProduct.title,
+                          description: _editedProduct.description,
+                          price: _editedProduct.price,
+                          imageUrl: value as String,
+                        );
+                      },
+                      onFieldSubmitted: (_) => _saveForm(),
                     ),
                   ),
                 ],
