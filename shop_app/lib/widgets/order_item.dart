2fefcb97db8e8abc6d07f33a13cf9852fff856bd
vtrfaas <vitor.f.arantes@gmail.com>
2021-09-15 09:17:36 -0300

    Criado um container animado baseado na altura para os cards das ordens de compra

--- shop_app/lib/widgets/order_item.dart
+++ shop_app/lib/widgets/order_item.dart
@@ -20,29 +20,41 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            //formatamos a data e hora do nosso jeito
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      //a animação será na altura
+      //o main container deve ter uma base height de 95
+      //lembrando que nosso main container deve ser maior que secundário
+      height:
+          _expanded ? min(widget.order.products.length * 22.0 + 110, 200) : 95,
+
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              //formatamos a data e hora do nosso jeito
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              ),
+              trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-            ),
-          ),
-          if (_expanded)
-            Container(
+            // if (_expanded)
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(horizontal: 10),
-              height: min(widget.order.products.length * 22.0 + 10, 180),
+              //por conta da remoção do if, esse container não deve aparecer senão for expandido
+              height: _expanded
+                  ? min(widget.order.products.length * 22.0 + 10, 180)
+                  : 0,
               child: ListView(
                 children: [
                   ...widget.order.products
@@ -71,7 +83,8 @@ class _OrderItemState extends State<OrderItem> {
                 ],
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
